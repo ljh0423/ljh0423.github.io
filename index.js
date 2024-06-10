5007f37de8356e153488c9cac29f36efdc8ec374
Juhyun Lee <78312165+ljh0423@users.noreply.github.com>
2024-06-10 00:54:46 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 // index.js
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import App from './app.js';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const domNode = document.getElementById('root');
+const root = createRoot(domNode);
+root.render(<App />);
+
+// ReactDOM.render(<App />, document.getElementById('root'));
