377360928544bd69ca1d14d240fb4fd09a6b9ee4
Juhyun Lee <78312165+ljh0423@users.noreply.github.com>
2024-06-10 00:11:31 -0400

    Create app.js

---
+++ app.js
@@ -0,0 +1,43 @@
+// App.js
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Juhyun Lee</h1>
+        <p>Welcome to my website!</p>
+      </header>
+      <nav>
+        <ul>
+          <li><a href="#">Home</a></li>
+          <li><a href="#">NLP</a></li>
+          <li><a href="#">2D Game</a></li>
+          <li><a href="#">Chatbot</a></li>
+          <li><a href="#">Three.js</a></li>
+        </ul>
+      </nav>
+      <div className="container">
+        <div className="row">
+          <div className="col-sm-4">
+            <h2>About Me</h2>
+            <p>I am an aspiring software developer who likes to travel the universe. I would like to experience more throughout this journey, and I hope you can be a part of it too.</p>
+          </div>
+          <div className="col-sm-8">
+            <h2>Tic-Tac-Toe</h2>
+            <p>Click on the link to play <a href="ttt.html">Tic-Tac-Toe</a></p>
+            <p>See if you can figure out a strategy.</p>
+          </div>
+        </div>
+      </div>
+      <footer>
+        <div className="footer-content">
+          <p>Tel.(+1)519-781-8962 | Adr.300 Ginseng St. Waterloo | <a href="mailto:j866lee@uwaterloo.ca">j866lee@uwaterloo.ca</a></p>
+        </div>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
